995aff4d42c4c008abf83c714b4ec6cf06dc3106
Damil Imran <damil.imran05@gmail.com>
2024-10-15 23:39:21 +0100

    Changed model Added file upload to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
 import "./App.css";
 
+import { useCallback, useState } from 'react';
+import { useDropzone} from 'react-dropzone';
+
+
+import FormRow from "./components/FormRow";
+import FormLabel from "./components/FormLabel";
+import InputText from "./components/InputText";
+
+
 function App() {
   const handleRequest = async() =>{
     try {
@@ -11,14 +20,65 @@ function App() {
       }
 
   }
+
+
+  const onDrop = useCallback((acceptedFiles: Array<File>) => {
+    const file = new FileReader;
+
+    file.onload = function() {
+      setPreview(file.result);
+    }
+
+    file.readAsDataURL(acceptedFiles[0])
+  }, [])
+
+  const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop
+  });
+
+  const [preview, setPreview] = useState<string | ArrayBuffer | null>(null);
+
+  /**
+   * handleOnSubmit
+   */
+  async function handleOnSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    console.log("bosh");
+  }
+
+
   return (
-    <>
-      <div>
-        <h1>Kitty cat identifier</h1>
-        <button onClick={handleRequest}>Put hello in console</button>
-      </div>
-    </>
-  );
-}
+
+      <>
+        <h1 className="text-6xl font-black text-center text-slate-900 mb-20">
+          Kitty cat identifier
+        </h1>
+        
+        <form id="myForm" name="myForm" className="max-w-md border border-gray-200 rounded p-6 mx-auto" onSubmit={handleOnSubmit}>
+
+          <FormRow className="mb-5">
+            <FormLabel htmlFor="image"></FormLabel>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              {
+                isDragActive ?
+                  <p>Drop the files here ...</p> :
+                  <p>Drag 'n' drop some files here, or click to select files</p>
+              }
+            </div>
+          </FormRow>
+
+          {preview && (
+            <p className="mb-5">
+              <img src={preview as string} alt="Upload preview" />
+            </p>
+          )}
+
+          <button>Submit</button>
+        </form>
+
+      </>
+
+)}
 
 export default App;
